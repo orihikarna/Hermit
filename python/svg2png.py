edc99902ab41d700c2ca5ed687a831d075e76685
orihikarna <orihikarna@gmail.com>
2021-05-05 15:43:39 +0900

    fixed distance image

--- python/svg2png.py
+++ python/svg2png.py
@@ -7,60 +7,63 @@ board = 'ZoeaT'
 board = 'ZoeaB'
 board = 'HermitT'
 board = 'HermitB'
+layer = 'Edge_Cuts'
 root = '/Users/akihiro/repos/Hermit/{}/'.format( board )
-path_svg = root + 'layer/{}-Edge_Cuts.svg'.format( board )
+path_svg = root + 'layer/{}-{}.svg'.format( board, layer )
 path_png = root + 'svg.png'
 path_txt = root + 'Edge_Fill.txt'
 path_dist = root + 'dist.png'
 cairosvg.svg2png( url = path_svg, write_to = path_png, dpi = 254 )
+if True:
+    png = Image.open( path_png )
+    cairosvg.svg2png( url = path_svg, write_to = path_png, dpi = 254,
+        output_width = png.size[0] + 2, output_height = png.size[1] + 2 )
 
 png = Image.open( path_png )
 print( f'png size = {png.size}' )
 # voi: x width
-x0 = png.size[0]
-x1 = 0
-for x in range( png.size[0] ):
-    for y in range( 0, png.size[1], 5 ):
-        _, _, _, a = png.getpixel( (x, y) )
+ix0 = png.size[0]
+ix1 = 0
+for ix in range( png.size[0] ):
+    for iy in range( 0, png.size[1], 5 ):
+        _, _, _, a = png.getpixel( (ix, iy) )
         a = 255 if a >= 128 else 0
         if a == 255:
-            x0 = min( x0, x )
-            x1 = max( x1, x )
+            ix0 = min( ix0, ix )
+            ix1 = max( ix1, ix )
             break
-x0 -= 1
-x1 += 1
-# x0 = max( x0, 0 )
-# x1 = min( x1, png.size[0] - 1 )
-w = x1 - x0
-print( "x = ({}, {}), width = {}".format( x0, x1, w ) )
+ix0 -= 1
+ix1 += 1
+w = ix1 - ix0 + 1
+print( "ix = ({}, {}), width = {}".format( ix0, ix1, w ) )
 
 # voi: y height
-y0 = png.size[1]
-y1 = 0
-for y in range( png.size[1] ):
-    for x in range( 0, png.size[0], 5 ):
-        _, _, _, a = png.getpixel( (x, y) )
+iy0 = png.size[1]
+iy1 = 0
+for iy in range( png.size[1] ):
+    for ix in range( 0, png.size[0], 5 ):
+        _, _, _, a = png.getpixel( (ix, iy) )
         a = 255 if a >= 128 else 0
         if a == 255:
-            y0 = min( y0, y )
-            y1 = max( y1, y )
+            iy0 = min( iy0, iy )
+            iy1 = max( iy1, iy )
             break
-y0 -= 1
-y1 += 1
-# y0 = max( y0, 0 )
-# y1 = min( y1, png.size[1] - 1 )
-h = y1 - y0
-print( "y = ({}, {}), height = {}".format( y0, y1, h ) )
+iy0 -= 1
+iy1 += 1
+h = iy1 - iy0 + 1
+print( "iy = ({}, {}), height = {}".format( iy0, iy1, h ) )
 
 mask = np.zeros( (h, w), np.uint8 )
-for y in range( y0, y1 ):
-    for x in range( x0, x1 ):
-        if 0 <= x and x < png.size[0] and 0 <= y and y < png.size[1]:
-            _, _, _, a = png.getpixel( (x, y) )
+for y in range( h ):
+    iy = y + iy0
+    for x in range( w ):
+        ix = x + ix0
+        if 0 <= ix and ix < png.size[0] and 0 <= iy and iy < png.size[1]:
+            _, _, _, a = png.getpixel( (ix, iy) )
         else:
             a = 0
         v = 0 if a >= 64 else 255
-        mask[y - y0, x - x0] = v
+        mask[y, x] = v
 
 if False:
     retval, labels = cv2.connectedComponents( mask )
@@ -82,11 +85,11 @@ else:
 
 mask1 = np.zeros( (h, w), np.uint8 )
 mask2 = np.zeros( (h, w), np.uint8 )
-for y in range( y0, y1 ):
-    for x in range( x0, x1 ):
-        inside = (labels[y - y0, x - x0] == label)
-        mask1[y - y0, x - x0] = 255 if inside else 0
-        mask2[y - y0, x - x0] = 0 if inside else 255
+for y in range( h ):
+    for x in range( w ):
+        inside = (labels[y, x] == label)
+        mask1[y, x] = 255 if inside else 0
+        mask2[y, x] = 0 if inside else 255
 print( 'done largest label mask' )
 
 dist1 = cv2.distanceTransform( mask1, cv2.DIST_L2, cv2.DIST_MASK_PRECISE )
@@ -97,19 +100,19 @@ print( 'done distance transform' )
 
 # dist = Image.new( 'RGB', (w, h) )
 dist = np.empty((h, w, 3), np.uint8)
-for y in range( y0, y1 ):
-    for x in range( x0, x1 ):
-        inside = True if mask1[y - y0, x - x0] != 0 else False
-        val = dist1[y - y0, x - x0] if inside else dist2[y - y0, x - x0]
+for y in range( h ):
+    for x in range( w ):
+        inside = True if mask1[y, x] != 0 else False
+        val = dist1[y, x] if inside else dist2[y, x]
         val = int( round( val * 3 ) )
         val = min( max( val, 0), 255 )
         r = 255 if inside else 0
         b = 0 if inside else 255
         g = val
-        # dist.putpixel( (x - x0, y - y0), (r, g, b))
-        dist[y - y0, x - x0, 0] = r
-        dist[y - y0, x - x0, 1] = g
-        dist[y - y0, x - x0, 2] = b
+        # dist.putpixel( (x, y), (r, g, b))
+        dist[y, x, 0] = r
+        dist[y, x, 1] = g
+        dist[y, x, 2] = b
 dist = Image.fromarray( dist )
 dist.save( path_dist )
 print( f'saved {path_dist}' )
@@ -118,11 +121,11 @@ print( f'saved {path_dist}' )
 with open( path_txt, 'w' ) as fout:
     fout.write( '{}\n'.format( w ) )
     fout.write( '{}\n'.format( h ) )
-    for y in range( y0, y1 ):
+    for y in range( h ):
         line = ''
-        for x in range( x0, x1 ):
-            inside = True if mask1[y - y0, x - x0] != 0 else False
-            val = dist1[y - y0, x - x0] if inside else -dist2[y - y0, x - x0]
+        for x in range( w ):
+            inside = True if mask1[y, x] != 0 else False
+            val = dist1[y, x] if inside else -dist2[y, x]
             line += '{:.0f},'.format( val * 10 )
         fout.write( line + '\n' )
 print( f'saved {path_txt}' )
